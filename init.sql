fa501bdde18a23224a1fc3cf0423208edc634769
Jessica <jessicabpetersen041ster@gmail.com>
2024-11-11 18:14:04 -0300

    criacao das tabelas

--- init.sql
+++ init.sql
@@ -1,5 +1,23 @@
 USE test_db;
 
 --TODO Crie a tabela de user;
-
+CREATE TABLE IF NOT EXISTS public.user (
+	id integer NOT NULL AUTO_INCREMENT,
+	firstName Varchar(100) NOT NULL,
+    lastName  Varchar(100) NOT NULL,
+	email   Varchar(100) NOT NULL,
+    CONSTRAINT user_pkey PRIMARY KEY (id)
+);
 --TODO Crie a tabela de posts;
+CREATE TABLE IF NOT EXISTS public.post (
+	id integer NOT NULL AUTO_INCREMENT,
+	title Varchar(100) NOT NULL,
+    description  Varchar(100) NOT NULL,
+	userId integer,
+    CONSTRAINT user_pkey PRIMARY KEY (id)
+	CONSTRAINT userId FOREIGN KEY (userId)
+        REFERENCES public.user (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID
+);
