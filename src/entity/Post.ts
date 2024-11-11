791c161f46f2ae275354ac5a1847749ba5dae135
Jessica <jessicabpetersen041ster@gmail.com>
2024-11-11 18:26:43 -0300

    adicionado entidades e pacotes do node

--- src/entity/Post.ts
+++ src/entity/Post.ts
@@ -1,3 +1,19 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne  } from "typeorm";
+import { User } from "./User";
 
 //TODO Crie a entidade de Post
+
+@Entity()
+export class Post {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    title: string;
+
+    @Column()
+    description: string;
+
+    @ManyToOne(() => User, (user) => user.posts)
+    user: User;
+}
